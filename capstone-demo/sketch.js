152a6f4b8768bd5ca915567ab719862bcb2299d1
Michael Leo Kokkat <54603696+mlk525@users.noreply.github.com>
2023-02-28 18:41:44 +0400

    Update sketch.js

--- capstone-demo/sketch.js
+++ capstone-demo/sketch.js
@@ -1,182 +1,365 @@
-let img;
+
 let video;
 let poseNet;
 let noseX = 0;
 let noseY = 0;
+let eyelX = 0;
+let eyelY = 0;
 
-let myRec = new p5.SpeechRec();
+let fnoseX = 0,
+    fnoseY = 0;
 
-let eyes;
-let x = -5;
-let commandFoo = false;
+let isLoaded = false;
+let glitch_front;
+let glitch_text;
+let mona_front ='mona lisa_front.png';
+let mona_eyes = 'mona lisa_eyes.png';
+let mona_back = 'mona lisa_back.png';
+let mona_text = 'mona lisa_text.png';
+let glitched = false;
+let debug = false;
+
+let x,y;
 
 function preload() {
-  back = loadImage("van gogh_back.png");
-  eyeL = loadImage("van gogh_eyeL.png");
-  eyeR = loadImage("van gogh_eyeR.png");
-  front = loadImage("https://i.postimg.cc/L85mVSDp/van-gogh-front.png");
+  front = loadImage(mona_front);
+  eyes = loadImage(mona_eyes);
+  back = loadImage(mona_back);
+  loadImage(mona_front, function(img) {
+        glitch_front = new Glitch(img);
+        isLoaded = true;
+    });
+  loadImage(mona_text, function(img) {
+        glitch_text = new Glitch(img);
+        isLoaded = true;
+    });
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  imageMode(CENTER);
-  video = createCapture(VIDEO);
-  video.hide();
-  poseNet = ml5.poseNet(video, modelReady);
-  poseNet.on("pose", gotPoses);
+    background(0);
   
-  myRec.onResult = readResult;
-  
-  myRec.continuous = true;
-  myRec.interimResults = false;
-  myRec.start();
-}
-
-function readResult(){   
-  str1 = myRec.resultString;
-  console.log(str1);
-  
-  if(str1.includes("roll your eyes")){
-    commandFoo = true;
-  }
-
+    createCanvas(windowWidth, windowHeight);
+    
+    video = createCapture(VIDEO);
+    video.hide();
+    poseNet = ml5.poseNet(video);
+    poseNet.on('pose', gotPoses);
 }
 
 function gotPoses(poses) {
-  // console.log(poses);
   if (poses.length > 0) {
     let nX = poses[0].pose.keypoints[0].position.x;
     let nY = poses[0].pose.keypoints[0].position.y;
-    //let eX = poses[0].pose.keypoints[1].position.x;
-    //let eY = poses[0].pose.keypoints[1].position.y;
+    let eX = poses[0].pose.keypoints[1].position.x;
+    let eY = poses[0].pose.keypoints[1].position.y;
     noseX = lerp(noseX, nX, 0.5);
     noseY = lerp(noseY, nY, 0.5);
-    //eyelX = lerp(eyelX, eX, 0.5);
-    //eyelY = lerp(eyelY, eY, 0.5);
+    eyelX = lerp(eyelX, eX, 0.5);
+    eyelY = lerp(eyelY, eY, 0.5);
   }
-}
-
-function modelReady() {
-  console.log("model ready");
 }
 
 function draw() {
-  //image(video, 0, 0);
+  let d = dist(noseX, noseY, eyelX, eyelY);
+  console.log(d);
   
-  if(commandFoo){
-    eyes = rollEyes();
-  }
+  fnoseX = map(noseX, 0, video.width, width, 0);
+  fnoseY = map(noseY, 0, video.height, 0, height);
+  x = map(fnoseX, 0, width, -14, 2);
+  y = map(fnoseY, 0, height, -2, 6);
   
-  else{
-    eyes = trackEyes();
-  }
-  
-  
-  let eyeLX = eyes.eyeLX,
-      eyeLY = eyes.eyeLY,
-      eyeRX = eyes.eyeRX,
-      eyeRY = eyes.eyeRY;
+  push();
+  imageMode(CENTER);
+  image(back, width/2,height/2);
+  image(eyes, width/2 + x,height/2 + y);
+  image(front, width/2,height/2);
+  pop();
 
-  if (back.width / back.height < width / height) {
-    image(
-      back,
-      width / 2,
-      height / 2,
-      width,
-      (width / back.width) * back.height
-    );
+  if ((isLoaded) && ((d > 40) || (glitched))) {
 
-    image(eyeL, eyeLX, eyeLY, width, (width / back.width) * back.height);
-    image(eyeR, eyeRX, eyeRY, width, (width / back.width) * back.height);
-
-    image(
-      front,
-      width / 2,
-      height / 2,
-      width,
-      (width / back.width) * back.height
-    );
-
-    console.log("case 1");
-    //console.log(width-noseX, noseY)
-  } else if (back.width / back.height > width / height) {
-    image(
-      back,
-      width / 2,
-      height / 2,
-      (height / back.height) * back.width,
-      height
-    );
-
-    image(eyeL, eyeLX, eyeLY, (height / back.height) * back.width, height);
-    image(eyeR, eyeRX, eyeRY, (height / back.height) * back.width, height);
-
-    image(
-      front,
-      width / 2,
-      height / 2,
-      (height / back.height) * back.width,
-      height
-    );
-
-    console.log("case 2");
-  } else {
-    image(back, width / 2, height / 2, width, height);
-  }
+    glitch_front.show();
     
+    if (d > 70){
+      glitch_text.show();
+    }
+  }
+  
+  if (debug){
+    fill(0,0,255);
+    ellipse(width-fnoseX, fnoseY, d);
+  }
+
+  // fill(255, 255, 255);
+  // textSize(14);
+  // text('FPS: ' + floor(frameRate()), 20, 30);
 
 }
 
-function trackEyes(){
+class Glitch {
+    constructor(img) {
+        this.channelLen = 4;
+        this.imgOrigin = img;
+        this.imgOrigin.loadPixels();
+        this.copyData = [];
+        this.flowLineImgs = [];
+        this.shiftLineImgs = [];
+        this.shiftRGBs = [];
+        this.scatImgs = [];
+        this.throughFlag = true;
+        this.copyData = new Uint8ClampedArray(this.imgOrigin.pixels);
 
-  let eyeLX = map(width - noseX, 0, back.width, width / 2 - 5, width / 2 + 10),
-  eyeLY = map(noseY, 0, back.height, height / 2, height / 2 + 10),
-  eyeRX = map(width - noseX, 0, back.width, width / 2 - 5, width / 2 + 10),   eyeRY = map(noseY, 0, back.height, height / 2, height / 2 + 10);
-  
-  return {eyeLX, eyeLY, eyeRX, eyeRY};
+        // flow line
+        for (let i = 0; i < 1; i++) {
+            let o = {
+                pixels: null,
+                t1: floor(random(0, 1000)),
+                speed: floor(random(4, 24)),
+                randX: floor(random(24, 80))
+            };
+            this.flowLineImgs.push(o);
+        }
+
+        // shift line
+        for (let i = 0; i < 6; i++) {
+            let o = null;
+            this.shiftLineImgs.push(o);
+        }
+
+        // shift RGB
+        for (let i = 0; i < 1; i++) {
+            let o = null;
+            this.shiftRGBs.push(o);
+        }
+
+        // scat imgs
+        for (let i = 0; i < 3; i++) {
+            let scatImg = {
+                img: null,
+                x: 0,
+                y: 0
+            };
+            this.scatImgs.push(scatImg);
+        }
+    }
+
+    replaceData(destImg, srcPixels) {
+        for (let y = 0; y < destImg.height; y++) {
+            for (let x = 0; x < destImg.width; x++) {
+                let r, g, b, a;
+                let index;
+                index = (y * destImg.width + x) * this.channelLen;
+                r = index;
+                g = index + 1;
+                b = index + 2;
+                a = index + 3;
+                destImg.pixels[r] = srcPixels[r];
+                destImg.pixels[g] = srcPixels[g];
+                destImg.pixels[b] = srcPixels[b];
+                destImg.pixels[a] = srcPixels[a];
+            }
+        }
+        destImg.updatePixels();
+    }
+
+    flowLine(srcImg, obj) {
+
+        let destPixels,
+            tempY;
+        destPixels = new Uint8ClampedArray(srcImg.pixels);
+        obj.t1 %= srcImg.height;
+        obj.t1 += obj.speed;
+        //tempY = floor(noise(obj.t1) * srcImg.height);
+        tempY = floor(obj.t1);
+        for (let y = 0; y < srcImg.height; y++) {
+            if (tempY === y) {
+                for (let x = 0; x < srcImg.width; x++) {
+                    let r, g, b, a;
+                    let index;
+                    index = (y * srcImg.width + x) * this.channelLen;
+                    r = index;
+                    g = index + 1;
+                    b = index + 2;
+                    a = index + 3;
+                    destPixels[r] = srcImg.pixels[r] + obj.randX;
+                    destPixels[g] = srcImg.pixels[g] + obj.randX;
+                    destPixels[b] = srcImg.pixels[b] + obj.randX;
+                    destPixels[a] = srcImg.pixels[a];
+                }
+            }
+        }
+        return destPixels;
+    }
+
+    shiftLine(srcImg) {
+
+        let offsetX;
+        let rangeMin, rangeMax;
+        let destPixels;
+        let rangeH;
+
+        destPixels = new Uint8ClampedArray(srcImg.pixels);
+        rangeH = srcImg.height;
+        rangeMin = floor(random(0, rangeH));
+        rangeMax = rangeMin + floor(random(1, rangeH - rangeMin));
+        offsetX = this.channelLen * floor(random(-40, 40));
+
+        for (let y = 0; y < srcImg.height; y++) {
+            if (y > rangeMin && y < rangeMax) {
+                for (let x = 0; x < srcImg.width; x++) {
+                        let r, g, b, a;
+                        let r2, g2, b2, a2;
+                        let index;
+
+                        index = (y * srcImg.width + x) * this.channelLen;
+                        r = index;
+                        g = index + 1;
+                        b = index + 2;
+                        a = index + 3;
+                        r2 = r + offsetX;
+                        g2 = g + offsetX;
+                        b2 = b + offsetX;
+                        destPixels[r] = srcImg.pixels[r2];
+                        destPixels[g] = srcImg.pixels[g2];
+                        destPixels[b] = srcImg.pixels[b2];
+                        destPixels[a] = srcImg.pixels[a];
+                }
+            }
+        }
+        return destPixels;
+    }
+
+    shiftRGB(srcImg) {
+
+        let randR, randG, randB;
+        let destPixels;
+        let range;
+
+        range = 16;
+        destPixels = new Uint8ClampedArray(srcImg.pixels);
+        randR = (floor(random(-range, range)) * srcImg.width + floor(random(-range, range))) * this.channelLen;
+        randG = (floor(random(-range, range)) * srcImg.width + floor(random(-range, range))) * this.channelLen;
+        randB = (floor(random(-range, range)) * srcImg.width + floor(random(-range, range))) * this.channelLen;
+
+        for (let y = 0; y < srcImg.height; y++) {
+            for (let x = 0; x < srcImg.width; x++) {
+                let r, g, b, a;
+                let r2, g2, b2, a2;
+                let index;
+
+                index = (y * srcImg.width + x) * this.channelLen;
+                r = index;
+                g = index + 1;
+                b = index + 2;
+                a = index + 3;
+                r2 = (r + randR) % srcImg.pixels.length;
+                g2 = (g + randG) % srcImg.pixels.length;
+                b2 = (b + randB) % srcImg.pixels.length;
+                destPixels[r] = srcImg.pixels[r2];
+                destPixels[g] = srcImg.pixels[g2];
+                destPixels[b] = srcImg.pixels[b2];
+                destPixels[a] = srcImg.pixels[a];
+            }
+        }
+
+        return destPixels;
+    }
+
+    getRandomRectImg(srcImg) {
+        let startX;
+        let startY;
+        let rectW;
+        let rectH;
+        let destImg;
+        startX = floor(random(0, srcImg.width - 30));
+        startY = floor(random(0, srcImg.height - 50));
+        rectW = floor(random(30, srcImg.width - startX));
+        rectH = floor(random(1, 50));
+        destImg = srcImg.get(startX, startY, rectW, rectH);
+        destImg.loadPixels();
+        return destImg;
+    }
+
+    show() {
+      
+        // restore the original state
+        this.replaceData(this.imgOrigin, this.copyData);
+
+        // sometimes pass without effect processing
+        let n = floor(random(100));
+        if (n > 75 && this.throughFlag) {
+            this.throughFlag = false;
+            setTimeout(() => {
+                this.throughFlag = true;
+            }, floor(random(40, 400)));
+        }
+        if (!this.throughFlag) {
+            push();
+            translate((width - this.imgOrigin.width) / 2, (height - this.imgOrigin.height) / 2);
+            image(this.imgOrigin, 0,0);
+            pop();
+            return;
+        }
+
+        // flow line
+        this.flowLineImgs.forEach((v, i, arr) => {
+            arr[i].pixels = this.flowLine(this.imgOrigin, v);
+            if (arr[i].pixels) {
+                this.replaceData(this.imgOrigin, arr[i].pixels);
+            }
+        })
+
+        // shift line
+        this.shiftLineImgs.forEach((v, i, arr) => {
+            if (floor(random(100)) > 50) {
+                arr[i] = this.shiftLine(this.imgOrigin);
+                this.replaceData(this.imgOrigin, arr[i]);
+            } else {
+                if (arr[i]) {
+                    this.replaceData(this.imgOrigin, arr[i]);
+                }
+            }
+        })
+
+        // shift rgb
+        this.shiftRGBs.forEach((v, i, arr) => {
+            if (floor(random(100)) > 65) {
+                arr[i] = this.shiftRGB(this.imgOrigin);
+                this.replaceData(this.imgOrigin, arr[i]);
+            }
+        })
+
+        push();
+        translate((width - this.imgOrigin.width) / 2, (height - this.imgOrigin.height) / 2);
+        image(this.imgOrigin, 0,0);
+        pop();
+
+        // scat image
+        this.scatImgs.forEach((obj) => {
+            push();
+            translate((width - this.imgOrigin.width) / 2, (height - this.imgOrigin.height) / 2);
+            if (floor(random(100)) > 80) {
+                obj.x = floor(random(-this.imgOrigin.width * 0.3, this.imgOrigin.width * 0.7));
+                obj.y = floor(random(-this.imgOrigin.height * 0.1, this.imgOrigin.height));
+                obj.img = this.getRandomRectImg(this.imgOrigin);
+            }
+            if (obj.img) {
+                image(obj.img, obj.x, obj.y);
+            }
+            pop();
+        })
+
+    }
+
 }
 
-function rollEyes(){
+function keyPressed(){
   
-  let eyeLX = width/2,
-      eyeLY = height/2,
-      eyeRX = width/2,
-      eyeRY = height/2;
-  
-  if (x < 8){
-    let y = 4-(x*x/8);
-    
-    eyeLX -= x;
-    eyeRX -= x;
-    
-    eyeLY -= y;
-    eyeRY -= y;
-    
-    x += 1;
-    
-    return {eyeLX, eyeLY, eyeRX, eyeRY};
-    
+  console.log(key);
+  if ((key == 'd') || (key == 'D')) {
+    debug = !debug;
+  }
+  if ((key == 'g') || (key == 'G')) {
+    glitched = !glitched;
   }
   
-  eyeLX = width/2-5;
-  eyeLY = height/2+2;
-  eyeRX = width/2-5;
-  eyeRY = height/2+2;
-  
-  commandFoo = false;
-  x = -5;
-  return {eyeLX, eyeLY, eyeRX, eyeRY};
-}
-
-function sleep(milliseconds) {
-  
-  const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
-}
-
-function mouseClicked() {
-  commandFoo = true;
 }
